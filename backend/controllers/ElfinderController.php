6f8aa2ea41567101112873760fb3134c5f5b8bf9
Yuri Stopchik <yastopch@gmail.com>
2019-07-15 09:18:58 +0300

    step 11 fix file-manager

--- backend/controllers/ElfinderController.php
+++ backend/controllers/ElfinderController.php
@@ -9,18 +9,40 @@
 namespace backend\controllers;
 
 use mihaildev\elfinder\Controller;
+use Yii;
 
 /**
  * Class ElfinderController
  * Ovveride Elfinder controller method actionManager
  * @package backend\controllers
  */
-class ElfinderController extends BackendController
+class ElfinderController extends Controller
 {
     public function actionManager(){
         return $this->renderFile("@backend/views/file-manager/manager.php", [
             'options'=>$this->getManagerOptions(),
             'startPathHash'=> \Yii::$app->request->getQueryParam('target', false)
         ]);
+    }
+
+    public function beforeAction($action)
+    {
+        $user=Yii::$app->user->identity;
+
+        if($user !== null){
+            $auth = Yii::$app->authManager;
+
+            $roles = $auth->getRolesByUser($user->getId());
+
+            foreach ($roles as $roleId=>$role){
+                if($roleId !== \common\models\User::ROLE_ADMINISTRATOR) {
+                    Yii::$app->user->logout();
+                    return $this->redirect(\Yii::getAlias('@frontendUrl'));
+                }
+            }
+
+        }
+
+        return parent::beforeAction($action);
     }
 }
